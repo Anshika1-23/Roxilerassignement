77cf6672f75bcbc31c90eeacafde64126e88bc54
Anshika <126404699+Anshika1-23@users.noreply.github.com>
2024-11-06 19:38:27 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,7 @@
-// src/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Adjust based on backend server port
+  baseURL: 'http://localhost:5000/api', 
 });
 
 export const fetchTransactions = (page, search, month) =>
